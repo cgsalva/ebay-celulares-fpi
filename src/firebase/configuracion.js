903d7cdfae4c4fa2e6ab046856abcc6467364dea
ElvisBanos <bm20001@ues.edu.sv>
2024-09-24 16:31:51 -0600

    Agregado Firebase

---
+++ src/firebase/configuracion.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from 'firebase/firestore/lite';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBMaA7Jfq2t-RWuDyxYWks1HPZ9begSILM",
+  authDomain: "ebay-celulares.firebaseapp.com",
+  projectId: "ebay-celulares",
+  storageBucket: "ebay-celulares.appspot.com",
+  messagingSenderId: "666276739177",
+  appId: "1:666276739177:web:aff83b37c44aa09141caf8",
+  measurementId: "G-N6817XZFZ3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const analytics = getAnalytics(app);
