957b5fac6d5e4feda464d09f716e2fec5849a0f1
ElvisBanos <bm20001@ues.edu.sv>
2024-09-24 20:45:56 -0600

    Firebase
    Se agrego la conexión con firebase.

--- src/components/FiltrosCelulares.vue
+++ src/components/FiltrosCelulares.vue
@@ -1,56 +1,87 @@
 <template>
   <div class="q-pa-md" style="width: 400px">
     <q-list bordered separator>
-    	<q-item v-ripple>
+      <q-item v-ripple>
         <q-item-section>
           <q-item-label>Filtros</q-item-label>
         </q-item-section>
       </q-item>
       <q-item clickable v-ripple>
-      	<q-item-section>
-        	<q-select v-model="modelPrecio" :options="precios" label="Precio" />
+        <!-- para las options esta todo en filtros por lo que solo hay que llamar al array indicado
+         en firebase se puede ver su nombre -->
+        <q-item-section>
+          <q-select v-model="modelPrecio" :options="precios" label="Precio" />
         </q-item-section>
       </q-item>
       <q-item clickable v-ripple>
         <q-item-section>
-        	<q-select v-model="modelMarca" :options="marcas" label="Marcas" />
-        </q-item-section>
-      </q-item>
-      <q-item clickable v-ripple>
-      	<q-item-section>
-        	<q-select v-model="model" :options="precios" label="Tamaño de pantalla" />
+          <q-select v-model="modelMarca" :options="filtros.marcas" label="Marcas" />
         </q-item-section>
       </q-item>
       <q-item clickable v-ripple>
         <q-item-section>
-        	<q-select v-model="model" :options="marcas" label="Sistema Operativo" />
+          <q-select v-model="modelPantalla" :options="precios" label="Tamaño de pantalla" />
         </q-item-section>
       </q-item>
       <q-item clickable v-ripple>
         <q-item-section>
-        	<q-select v-model="model" :options="marcas" label="Color" />
+          <q-select v-model="modelSO" :options="filtros.sistemaOperativo" label="Sistema Operativo" />
         </q-item-section>
       </q-item>
       <q-item clickable v-ripple>
         <q-item-section>
-        	<q-select v-model="model" :options="marcas" label="Almacenamiento Interno" />
+          <q-select v-model="modelColor" :options="marcas" label="Color" />
+        </q-item-section>
+      </q-item>
+      <q-item clickable v-ripple>
+        <q-item-section>
+          <q-select v-model="modelAlmacenamiento" :options="marcas" label="Almacenamiento Interno" />
         </q-item-section>
       </q-item>
     </q-list>
   </div>
+  <p>{{  filtros }}</p>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import { db } from '../firebase/configuracion.js'
+import { ref, onMounted } from 'vue' /* Importamos onMounted para poder  */
+import { db } from 'src/boot/firebase'
 import { collection, getDocs } from 'firebase/firestore/lite';
+
+const filtros = ref([])
 
 const modelPrecio = ref([''])
 
 const modelMarca = ref([''])
 
+const modelPantalla = ref([''])
+
+const modelSO = ref([''])
+
+const modelColor = ref([''])
+
+const modelAlmacenamiento = ref([''])
+
 const precios = ref(['$0.00 - $99.99', '$100 - $199.99', '$200 - Superior'])
 
-const marcas = ref(['Samsung', 'Nokia', 'LG'])
+onMounted(
+  async () => {
+    try {
+      const querySnapshot = await getDocs(collection(db, 'filtros'));
+      const filtrosArray = [];
+
+      querySnapshot.forEach((doc) => {
+        filtrosArray.push(doc.data());
+      });
+
+      filtros.value = filtrosArray[0]; // Asigna los datos a la variable reactiva
+      /* Se indica la posición 0 ya que devuleve un array donde cada posicion es un documento (object{})
+      y cada propiedad del object es un array (en este caso)*/
+    } catch (error) {
+      console.error('Error al obtener los filtros:', error);
+    }
+  }
+)
+
 
 </script>'
