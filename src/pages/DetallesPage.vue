5ef90d9c786d8ba8e333df26e04c40076632cd13
Gerardo <68165250+Pocachu@users.noreply.github.com>
2024-09-26 13:58:49 -0600

    cambios en la pagina de detalles
    cambios en la pagina de detalles, en el tamaño sm o inferior

--- src/pages/DetallesPage.vue
+++ src/pages/DetallesPage.vue
@@ -2,7 +2,7 @@
   <q-page>
     <div class="col-12">
       <div class="row">
-        <div class="col-7">
+        <div class="col-12 col-sm-7">
           <q-img
             class="q-ma-sm"
             name="img2"
@@ -10,7 +10,7 @@
             src="https://cdn.quasar.dev/img/parallax2.jpg"
           />
         </div>
-        <div class="col-5">
+        <div class="col-12 col-sm-5">
           <q-card class="my-card q-ma-sm" flat bordered style="width: auto">
             <q-card-section>
               <div class="col-12 text-h5 q-mb-xs">
@@ -22,9 +22,11 @@
                 Precio:(inserte union a db*)
               </div>
             </q-card-section>
+
+            <q-separator />
             <q-card-section>
               <div class="col-12 text-body1 q-mb-xs">
-                Descripción:(inserte union a db*)
+                Caracteristicas principales:(inserte union a db*)
               </div>
             </q-card-section>
             <q-card-section>
@@ -53,6 +55,21 @@
         </div>
       </div>
       <div class="row">
+        <div class="col-12">
+          <q-card class="my-card" flat bordered>
+            <q-card-section>
+              <div class="col-12 text-body1 q-mb-xs">
+                Mas informacion:(inserte union a db*)
+              </div>
+            </q-card-section>
+            <q-separator />
+            <q-card-section>
+              <div class="col-12 text-body1 q-mb-xs">
+                Texto detallado:(inserte union a db*)
+              </div>
+            </q-card-section>
+          </q-card>
+        </div>
         <div class="col-12">
           <div class="text-h5 q-ml-lg">Articulos similares</div>
         </div>
